37c8780981cc7c17f3e264113bf166b5ed05f4bf
abhishek-73 <abhishek.work02@gmail.com>
2016-01-07 01:16:26 +0530

    basic functionality is up. Basic functionality includes the following: 	writing code in C syntax. 	enabling user to edit text without deleting entire line. 	program compilation and error report and running.

---
+++ inc.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+
+function generate_c_file(){
+	rm -f $1 &> /dev/null
+	echo "#include <stdio.h>" >> $1
+	echo "#include <string.h>" >> $1
+	echo "#include <stdlib.h>" >> $1
+	echo "int main(int argc, char* argv[]) {" >> $1
+	cat ./temp >> $1
+	echo "return 0;" >> $1
+	echo "}" >> $1
+}
+
+function replace(){
+	echo "what = $what"
+	echo "with = $with"
+	sed -i -e 's/$what/$with/g' ./test_code.c
+}
+
+rm -f ./temp.bak
+touch temp.bak
+rm -f ./temp
+touch temp
+while :
+do
+	read -e input
+	case "$input" in
+		"");;
+		"run")
+			gcc ./test_code.c
+			./a.out
+			echo ""
+			;;
+		 *)
+			# count=`grep -c "$input"$ ./temp`
+			# if [ $count -eq 0 ]; then
+				echo $input >> temp
+				generate_c_file "test_code.c"
+				rm -f ./a.out
+				gcc ./test_code.c
+				if [ $? -eq 0 ]; then
+					count=`grep -c "printf(" ./test_code.c`
+					if [ $count -gt 0 ]; then
+						echo "Output ***********************"
+						./a.out
+						echo ""
+						echo "******************************"
+					fi
+					echo $input >> temp.bak
+				else
+					echo ""
+					echo ""
+					cp -f ./temp.bak ./temp	
+					generate_c_file "test_code.c"
+				fi
+			# fi
+			;;
+	esac
+done
