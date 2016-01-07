8ee4a95f869d958e6e91bbea70539040f869ad27
Abhishek Kr. Choubey <abhishek.choubey@inventum.net>
2016-01-07 11:45:24 +0530

    added new code

--- inc.sh
+++ inc.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-
+hold=0
 function generate_c_file(){
 	rm -f $1 &> /dev/null
 	echo "#include <stdio.h>" >> $1
@@ -12,47 +12,70 @@ function generate_c_file(){
 	echo "}" >> $1
 }
 
-function replace(){
-	echo "what = $what"
-	echo "with = $with"
-	sed -i -e 's/$what/$with/g' ./test_code.c
-}
-
 rm -f ./temp.bak
 touch temp.bak
 rm -f ./temp
 touch temp
+
 while :
 do
-	read -e input
+	read -p "C-In# " -e input 
 	case "$input" in
 		"");;
-		"run")
-			gcc ./test_code.c
-			./a.out
-			echo ""
-			;;
-		 *)
-				echo $input >> temp
-				generate_c_file "test_code.c"
-				rm -f ./a.out
-				gcc ./test_code.c
-				if [ $? -eq 0 ]; then
-					count=`grep -c "printf(" ./test_code.c`
-					if [ $count -gt 0 ]; then
-						echo "Output ***********************"
-						./a.out
-						echo ""
-						echo "******************************"
-						echo ""
-					fi
-					echo $input >> temp.bak
-				else
+	"exit")
+		exit 0
+		;;
+	"close")
+		exit 0
+		;;
+	"quit")
+		exit 0
+		;;
+	"hold")
+		hold=1
+		;;
+	"hold-")
+		hold=0
+		;;
+	"new")
+		# open new context
+		rm -f ./temp
+		rm -f ./temp.bak
+		rm -f ./test_code.c
+		touch ./temp
+		touch ./temp.bak
+		chmod 666 ./temp
+		chmod 666 ./temp.bak
+		reset
+		;;
+	"run")
+		./a.out
+		echo ""
+		;;
+	*)
+		echo $input >> temp
+		generate_c_file "test_code.c"
+		rm -f ./a.out
+		gcc ./test_code.c
+		if [ $? -eq 0 ]; then
+			echo $input >> temp.bak
+			count=`grep -c "printf(" ./test_code.c`
+			if [ $count -gt 0 ]; then
+				echo "printf(\"\\n\");" >> temp.bak
+				echo "printf(\"\\n\");" >> temp
+				if [ $hold -eq 0 ]; then
+					./a.out
 					echo ""
 					echo ""
-					cp -f ./temp.bak ./temp	
-					generate_c_file "test_code.c"
 				fi
-			;;
-	esac
+			fi
+		else
+			echo ""
+			echo ""
+			cp -f ./temp.bak ./temp	
+			generate_c_file "test_code.c"
+			gcc ./test_code.c
+		fi
+		;;
+esac
 done
